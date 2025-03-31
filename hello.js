c83d1b32559dbcf41a88444a2dbf6cd349c7b274
miilos <milos.pop.004@gmail.com>
2025-04-01 01:00:01 +0200

    test commit with vscode

--- hello.js
+++ hello.js
@@ -2,3 +2,8 @@ console.log("hello git!");
 console.log("second log");
 console.log("trying out revert");
 // SAVED THE DAY
+
+// I AM IN THE PROCESS OF IMPLEMENTING A VERY IMPORTANT FEATURE
+// CODE
+// CODE
+// CODE
